3567f548d949d9c9ad3d32d53966c02ca2545583
Hideto1112 <ueda_hideto@hotmail.com>
2021-02-11 06:55:00 +0000

    Finish add-basic-info

---
+++ db/migrate/20210211035802_add_department_to.rb
@@ -0,0 +1,4 @@
+class AddDepartmentTo < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210210013521) do
+ActiveRecord::Schema.define(version: 20210211042400) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20210210013521) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.string "department"
+    t.datetime "basic_time", default: "2021-02-10 23:00:00"
+    t.datetime "work_time", default: "2021-02-10 22:30:00"
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
