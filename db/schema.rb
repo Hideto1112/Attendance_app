8baec47a2ff5a75b586088d289e9f5f20fcc4c6b
Hideto1112 <ueda_hideto@hotmail.com>
2021-02-11 23:43:47 +0000

    Finish add-user-attendances

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210211042400) do
+ActiveRecord::Schema.define(version: 20210211091919) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.date "worked_on"
+    t.datetime "started_at"
+    t.datetime "finished_at"
+    t.string "note"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,8 +31,8 @@ ActiveRecord::Schema.define(version: 20210211042400) do
     t.string "password_digest"
     t.string "remember_digest"
     t.string "department"
-    t.datetime "basic_time", default: "2021-02-10 23:00:00"
-    t.datetime "work_time", default: "2021-02-10 22:30:00"
+    t.datetime "basic_time", default: "2021-02-11 23:00:00"
+    t.datetime "work_time", default: "2021-02-11 22:30:00"
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
